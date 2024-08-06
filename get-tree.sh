6cbc38b031032d0e00d1973a0a44a47007d8739a
Nitzan Raz <nitz.raz@gmail.com>
2024-08-06 22:03:59 +0300

    Added check_date and more fields, filtering by road closeness
    Summary:
    1. Filtering by trees that are close to walkable roads
    2. Added fields to the csv: lat,lon, last time validated

--- get-tree.sh
+++ get-tree.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -uo pipefail
 TREE_NAME="$1"
-CURL_OUT="$(curl -sS 'https://overpass-api.de/api/interpreter' --data '[out:json];node[natural=tree][species="'"$TREE_NAME"'"];out ;')"
+CURL_OUT="$(curl -sS 'https://overpass-api.de/api/interpreter' --data '[out:json];node["highway"~"living_street|pedestrian|residential"](around:10);node[natural=tree][species="'"$TREE_NAME"'"];out ;')"
 EX="$?"
+echo "$CURL_OUT"
 if [[ "$EX" != "0" ]] || ! (echo "$CURL_OUT" | jq . >/dev/null); then
   echo "error"
   echo "$CURL_OUT"  | head -n30 >&2
   exit $EX
 fi
 
-echo "$CURL_OUT" | jq '.elements[] | {"name": "'"$1"'", lat, lon, "osm": "https://www.openstreetmap.org/node/\(.id)", "streetview": "https://www.google.com/maps/@?api=1&map_action=pano&viewpoint=\(.lat),\(.lon)"}'
+echo "$CURL_OUT" | jq '.elements[] | {"name": "'"$1"'", lat, lon, "check_date": .tags.check_date ,"osm": "https://www.openstreetmap.org/node/\(.id)", "streetview": "https://www.google.com/maps/@?api=1&map_action=pano&viewpoint=\(.lat),\(.lon)"}'
